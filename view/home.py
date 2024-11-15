2756b507c9ff31cddfef1a9d03aadfb919f3cc4d
Jun An <junan@MacBook-Pro.local>
2024-11-15 16:04:29 +0800

    Form saving function works

--- view/home.py
+++ view/home.py
@@ -8,16 +8,16 @@ st.write(
 )
 
 # remove streamlit header and footer
-#hide_st_style = """
- #               <style>
- #               header {visibility: hidden;}
- #               footer {visibility: hidden;}
- #               </style>
- #               """
-#st.markdown(hide_st_style, unsafe_allow_html=True)
+# hide_st_style = """
+#               <style>
+#               header {visibility: hidden;}
+#               footer {visibility: hidden;}
+#               </style>
+#               """
+# st.markdown(hide_st_style, unsafe_allow_html=True)
 
 # Authenticate user
-#def authenticate():
+# def authenticate():
 #    if "authenticated" not in st.session_state:
 #        st.session_state.authenticated = False
 
@@ -31,46 +31,76 @@ st.write(
 
 #    return st.session_state.authenticated
 
-#if not authenticate():
+# if not authenticate():
 #    st.stop()
 
 # Load employee data
-df = pd.read_excel("partners.xlsx", index_col=0) # Set the first column as index
-
+df = pd.read_excel("partners.xlsx", index_col=0)  # Set the first column as index
+# print(df.columns)
 # Reset the index, start from 1
 df = df.reset_index(drop=True)
-df.index += 1 # Add 1 to each index
+df.index += 1  # Add 1 to each index
 
 col1, col2 = st.columns(2)
 with col1:
     # Search field
-    search_by = st.selectbox("Search By", ["", "Company", "Country", "Festivity", "Golf", "Name", "Reception", "BigS/SmallS", "Tier"], key="search_by")
+    search_by = st.selectbox(
+        "Search By",
+        [
+            "",
+            "Company",
+            "Country",
+            "Festivity",
+            "Golf",
+            "Name",
+            "Reception",
+            "BigS/SmallS",
+            "Tier",
+        ],
+        key="search_by",
+    )
 with col2:
     # Search query
-    search_query = st.text_input(f"Search {search_by}", key = "search_query")
-    filtered_df = df.copy() # store copy of current df into filtered_df
+    search_query = st.text_input(f"Search {search_by}", key="search_query")
+    filtered_df = df.copy()  # store copy of current df into filtered_df
 
 # Filter cards based on search by
 if search_query:
     if search_by == "":
         filtered_df = df.copy()
     elif search_by == "Name":
-        #search_query = st.selectbox(f"Search {search_by}", df[search_by].unique())
-        filtered_df = filtered_df[filtered_df['Name'].str.contains(search_query, case=False)]
+        # search_query = st.selectbox(f"Search {search_by}", df[search_by].unique())
+        filtered_df = filtered_df[
+            filtered_df["Name"].str.contains(search_query, case=False)
+        ]
     elif search_by == "Company":
-        filtered_df = filtered_df[filtered_df['Company'].str.contains(search_query, case=False)]
+        filtered_df = filtered_df[
+            filtered_df["Company"].str.contains(search_query, case=False)
+        ]
     elif search_by == "Country":
-        filtered_df = filtered_df[filtered_df['Country'].str.contains(search_query, case=False)]
+        filtered_df = filtered_df[
+            filtered_df["Country"].str.contains(search_query, case=False)
+        ]
     elif search_by == "Golf":
-        filtered_df = filtered_df[filtered_df['Golf'].str.contains(search_query, case=False)]
+        filtered_df = filtered_df[
+            filtered_df["Golf"].str.contains(search_query, case=False)
+        ]
     elif search_by == "Reception":
-        filtered_df = filtered_df[filtered_df['Reception'].str.contains(search_query, case=False)]
+        filtered_df = filtered_df[
+            filtered_df["Reception"].str.contains(search_query, case=False)
+        ]
     elif search_by == "Festivity":
-        filtered_df = filtered_df[filtered_df['Festivity'].str.contains(search_query, case=False)]
+        filtered_df = filtered_df[
+            filtered_df["Festivity"].str.contains(search_query, case=False)
+        ]
     elif search_by == "BigS/SmallS":
-        filtered_df = filtered_df[filtered_df['BigS/SmallS'].str.contains(search_query, case=False)]
+        filtered_df = filtered_df[
+            filtered_df["BigS/SmallS"].str.contains(search_query, case=False)
+        ]
     elif search_by == "Tier":
-        filtered_df = filtered_df[filtered_df['Tier'].str.lower() == search_query.lower()]
+        filtered_df = filtered_df[
+            filtered_df["Tier"].str.lower() == search_query.lower()
+        ]
 
 # Download the DataFrame as an Excel file
 current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
@@ -79,11 +109,13 @@ st.download_button(
     label="Download Data (.csv)",
     data=filtered_df.to_csv(),
     file_name=file_name,
-    mime="text/csv"
+    mime="text/csv",
 )
+
 
 def toggle_edit_mode(index):
     st.session_state[f"edit_mode_{index}"] = not st.session_state[f"edit_mode_{index}"]
+
 
 def get_tier_color(tier):
     # Define color mappings for different tiers
@@ -91,49 +123,56 @@ def get_tier_color(tier):
         "A+": "green",
         "A": "blue",
         "B": "purple",
-        "C": "orange"
+        "C": "orange",
         # Add more tiers and colors as needed
     }
     return tier_colors.get(tier, "gray")  # Default color for unknown tiers
 
+
 def get_status_color(status):
     return "green" if status == "Active" else "red"
 
+
 # Display employee cards setup with edit functionality
-def display_employee_card(employee_data,index):
-    employee_data = employee_data.fillna('Nil')
+def display_employee_card(employee_data, index):
+    # employee_data = employee_data.fillna("Nil")
+    # employee_data = employee_data.fillna(np.nan)
     with st.container():
 
         # Format the date to display only the date, handling potential missing values and invalid formats
-        posting_date = employee_data['Posting Date']
+        posting_date = employee_data["Posting Date"]
         formatted_posting_date = ""
-        if pd.notnull(posting_date) and posting_date != 'Nil':
+        if pd.notnull(posting_date):
             if isinstance(posting_date, str):
                 try:
-                    formatted_posting_date = datetime.strptime(posting_date, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+                    formatted_posting_date = datetime.strptime(
+                        posting_date, "%Y-%m-%d %H:%M:%S"
+                    ).strftime("%Y-%m-%d")
                 except ValueError:
                     formatted_posting_date = "Invalid Date"
             else:
-                formatted_posting_date = posting_date.strftime('%Y-%m-%d')
+                formatted_posting_date = posting_date.strftime("%Y-%m-%d")
 
-        deposted_date = employee_data['De-posted Date']
+        deposted_date = employee_data["De-posted Date"]
         formatted_deposted_date = ""
-        if pd.notnull(deposted_date) and deposted_date != 'Nil':
+        if pd.notnull(deposted_date) and deposted_date != "Nil":
             if isinstance(deposted_date, str):
                 try:
-                    formatted_deposted_date = datetime.strptime(deposted_date, '%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%d')
+                    formatted_deposted_date = datetime.strptime(
+                        deposted_date, "%Y-%m-%d %H:%M:%S"
+                    ).strftime("%Y-%m-%d")
                 except ValueError:
                     formatted_deposted_date = "Invalid Date"
             else:
-                formatted_deposted_date = deposted_date.strftime('%Y-%m-%d')
+                formatted_deposted_date = deposted_date.strftime("%Y-%m-%d")
 
         # Flag to track edit mode
         if f"edit_mode_{index}" not in st.session_state:
             st.session_state[f"edit_mode_{index}"] = False
-        
+
         # Read-only mode by default
         if not st.session_state[f"edit_mode_{index}"]:
-                
+
             st.markdown(
                 f"""
                 <div style="border-radius: 10px; border: 1px solid #ccc; padding: 10px; margin-bottom: 20px; background-color: #f9f9f9;">
@@ -173,72 +212,102 @@ def display_employee_card(employee_data,index):
                     </div>
                 </div>
                 """,
-                unsafe_allow_html=True
+                unsafe_allow_html=True,
             )
-            st.button(f"Edit {employee_data['Name']}'s info", key=f"edit_{index}", on_click=lambda: toggle_edit_mode(index))
+            st.button(
+                f"Edit {employee_data['Name']}'s info",
+                key=f"edit_{index}",
+                on_click=lambda: toggle_edit_mode(index),
+            )
         else:
             # Edit mode
+            print(st.session_state)
             with st.form(f"edit_form_{index}"):
                 # Create form fields for each detail
-                new_name = st.text_input("Name", value=employee_data['Name'])
-                new_designation = st.text_input("Designation", value=employee_data['Designation'])
-                new_status = st.text_input("Status", value=employee_data['Status'])
-                new_contact = st.text_input("Contact No.", value=employee_data['Contact No.'])
-                new_vehicle = st.text_input("Vehicle", value=employee_data['Vehicle'])
-                new_address = st.text_input("Address", value=employee_data['Address'])
-                new_posting_date = st.date_input("Posting Date (DD-MM-YY)", value=employee_data['Posting Date'])
-                new_depost_date = st.text_input("De-posted Date (YYYY/MM/DD)", value=employee_data['De-posted Date'])
-                new_golf = st.text_input("Golf", value=employee_data['Golf'])
-                new_golf_handicap = st.text_input("Golf Handicap", value=employee_data['Golf Handicap'])
-                new_dietary = st.text_input("Dietary Restrictions", value=employee_data['Dietary Restrictions'])
-                new_reception = st.text_input("Reception", value=employee_data['Reception'])
-                new_festivity = st.text_input("Festivity", value=employee_data['Festivity'])
-                new_interest = st.text_input("Interests", value=employee_data['Interests'])
-                new_tier = st.text_input("Tier", value=employee_data['Tier'])
-                new_sip = st.text_input("BigS/SmallS", value=employee_data['BigS/SmallS'])
-                
-                submitted = st.form_submit_button("Save")
+                new_name = st.text_input("Name", value=employee_data["Name"])
+                new_designation = st.text_input(
+                    "Designation", value=employee_data["Designation"]
+                )
+                new_status = st.text_input("Status", value=employee_data["Status"])
+                new_contact = st.text_input(
+                    "Contact No.", value=employee_data["Contact No."]
+                )
+                new_company = st.text_input("Company", value=employee_data["Company"])
+                new_country = st.text_input("Country", value=employee_data["Country"])
+                new_vehicle = st.text_input("Vehicle", value=employee_data["Vehicle"])
+                new_address = st.text_input("Address", value=employee_data["Address"])
+                new_posting_date = st.date_input(
+                    "Posting Date (DD-MM-YY)", value=employee_data["Posting Date"]
+                )
+                new_depost_date = st.text_input(
+                    "De-posted Date (YYYY/MM/DD)", value=employee_data["De-posted Date"]
+                )
+                new_golf = st.text_input("Golf", value=employee_data["Golf"])
+                new_golf_handicap = st.text_input(
+                    "Golf Handicap", value=employee_data["Golf Handicap"]
+                )
+                new_dietary = st.text_input(
+                    "Dietary Restrictions", value=employee_data["Dietary Restrictions"]
+                )
+                new_reception = st.text_input(
+                    "Reception", value=employee_data["Reception"]
+                )
+                new_festivity = st.text_input(
+                    "Festivity", value=employee_data["Festivity"]
+                )
+                new_interest = st.text_input(
+                    "Interests", value=employee_data["Interests"]
+                )
+                new_tier = st.text_input("Tier", value=employee_data["Tier"])
+                new_sip = st.text_input(
+                    "BigS/SmallS", value=employee_data["BigS/SmallS"]
+                )
+                submitted = st.form_submit_button("Submit")
 
+                print(st.session_state)
+                print(submitted)
                 if submitted:
-                    updates = { # currently not working as well
-                        "Name": new_name,
-                        "Designation": new_designation,
-                        "Contact No.": new_contact,
-                        "Vehicle": new_vehicle,
-                        "Address": new_address,
-                        "Posting Date": new_posting_date,
-                        "De-posted Date": new_depost_date,
-                        "Golf": new_golf,
-                        "Golf Handicap": new_golf_handicap,
-                        "Dietary Restrictions": new_dietary,
-                        "Reception": ", ".join(new_reception), # convert list to string format
-                        "Festivity": ", ".join(new_festivity),
-                        "Interests": new_interest,
-                        "Tier": new_tier,
-                        "BigS/SmallS": ", ".join(new_sip),
-                        "Status": new_status
-                    }
-                    df.loc[{index}] = updates
-                    df.to_excel("partners.xlsx", index=False)
-
+                    print("form submitted")
+                    print(new_name)
+                    df.at[index, "Name"] = new_name
+                    df.at[index, "Designation"] = new_designation
+                    df.at[index, "Status"] = new_status
+                    df.at[index, "Contact No."] = int(new_contact)
+                    df.at[index, "Company"] = new_company
+                    df.at[index, "Country"] = new_country
+                    df.at[index, "Vehicle"] = new_vehicle
+                    df.at[index, "Address"] = new_address
+                    df.at[index, "Posting Date"] = new_posting_date
+                    df.at[index, "De-posted Date"] = new_depost_date
+                    df.at[index, "Golf"] = str(new_golf)
+                    df.at[index, "Golf Handicap"] = int(new_golf_handicap)
+                    df.at[index, "Dietary Restrictions"] = new_dietary
+                    df.at[index, "Reception"] = new_reception
+                    df.at[index, "Festivity"] = new_festivity
+                    df.at[index, "Interests"] = new_interest
+                    df.at[index, "Tier"] = new_tier
+                    df.at[index, "BigS/SmallS"] = new_sip
+                    df.to_excel("partners.xlsx", index=True)
                     st.success("Employee information updated successfully!")
-                    st.session_state[f"edit_mode_{index}"] = False    
+                    st.session_state[f"edit_mode_{index}"] = False
                     st.rerun()
-                else:
-                    st.form_submit_button("Cancel")
-                    st.session_state[f"edit_mode_{index}"]=False
-            
+                # else:
+                #     st.form_submit_button("Cancel")
+                #     st.session_state[f"edit_mode_{index}"] = False
+
+
 # Display number of employee cards shown
 def display_employee_cards(df):
     num_cards = len(filtered_df)
     st.write(f"Showing {num_cards} results")
-    
+
     for index, employee_data in filtered_df.iterrows():
         display_employee_card(employee_data, index)
 
+
 # Display the filtered DataFrame
 with st.expander("List view"):
-    #st.write(filtered_df.drop('ID', axis=1))
+    # st.write(filtered_df.drop('ID', axis=1))
     st.write(filtered_df)
 
 # Display filtered or unfiltered data
